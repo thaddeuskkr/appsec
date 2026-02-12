873e6e70bbd8ae2719f9078acfef536bb63eb289
Thaddeus Kuah <tk@tkkr.dev>
2026-02-12 17:19:42 +0800

    fix: security
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- Services/SmtpEmailSender.cs
+++ Services/SmtpEmailSender.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 using BookwormsOnline.Options;
 using Microsoft.Extensions.Options;
 
@@ -16,19 +17,28 @@ public class SmtpEmailSender : IEmailSender
 
     public async Task SendAsync(string toEmail, string subject, string htmlBody, CancellationToken cancellationToken = default)
     {
+        if (!_options.UseSsl)
+        {
+            throw new InvalidOperationException("SMTP TLS/SSL is required for email delivery.");
+        }
+
         using var message = new MailMessage
         {
             From = new MailAddress(_options.FromAddress),
             Subject = subject,
             Body = htmlBody,
-            IsBodyHtml = true
+            IsBodyHtml = true,
+            BodyEncoding = Encoding.UTF8,
+            SubjectEncoding = Encoding.UTF8
         };
 
         message.To.Add(new MailAddress(toEmail));
 
         using var smtpClient = new SmtpClient(_options.Host, _options.Port)
         {
-            EnableSsl = _options.UseSsl,
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
             Credentials = new NetworkCredential(_options.Username, _options.Password)
         };
 
